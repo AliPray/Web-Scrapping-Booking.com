420cbf200c04f6c13d3d7143b8d54f092341e153
ali <ali.missaoui.jr@gmail.com>
2024-06-09 18:16:48 +0000

    ignore output folder

--- booking_scraper.py
+++ booking_scraper.py
@@ -9,7 +9,7 @@ def main():
         # IMPORTANT: Change dates to future dates, otherwise it won't work
         checkin_date = '2024-06-14'
         checkout_date = '2024-06-15'
-        
+
         page_url = f'https://www.booking.com/searchresults.html?ss=Dubai&ssne=Dubai&ssne_untouched=Dubai&label=gen173nr-1FCAEoggI46AdIM1gEaOIBiAEBmAExuAEXyAEM2AEB6AEB-AECiAIBqAIDuAKVv5KzBsACAdICJGZlM2NkMDA5LWI3MDItNDQzYy1hYTI1LTMyYjY2ZDg2NGU3NNgCBeACAQ&aid=304142&lang=en-us&sb=1&src_elem=sb&src=searchresults&dest_id=-782831&dest_type=city&checkin={checkin_date}&checkout={checkout_date}&group_adults=1&no_rooms=1&group_children=0'
 
         browser = p.chromium.launch(headless=False)
@@ -31,7 +31,7 @@ def main():
             hotels_list.append(hotel_dict)
         
         df = pd.DataFrame(hotels_list)
-        df.to_csv('hotels_list.csv', index=False) 
+        df.to_csv('output/hotels_list.csv', index=False) 
         
         browser.close()
             
