2e11c703c23137b41e0b59ccc2efaf85d1b2c5e1
ali <ali.missaoui.jr@gmail.com>
2024-06-09 03:17:03 +0000

    initial commit

---
+++ booking_scraper.py
@@ -0,0 +1,39 @@
+from playwright.sync_api import sync_playwright
+import pandas as pd
+
+
+def main():
+    
+    with sync_playwright() as p:
+        
+        # IMPORTANT: Change dates to future dates, otherwise it won't work
+        checkin_date = '2024-06-14'
+        checkout_date = '2024-06-15'
+        
+        page_url = f'https://www.booking.com/searchresults.html?ss=Dubai&ssne=Dubai&ssne_untouched=Dubai&label=gen173nr-1FCAEoggI46AdIM1gEaOIBiAEBmAExuAEXyAEM2AEB6AEB-AECiAIBqAIDuAKVv5KzBsACAdICJGZlM2NkMDA5LWI3MDItNDQzYy1hYTI1LTMyYjY2ZDg2NGU3NNgCBeACAQ&aid=304142&lang=en-us&sb=1&src_elem=sb&src=searchresults&dest_id=-782831&dest_type=city&checkin={checkin_date}&checkout={checkout_date}&group_adults=1&no_rooms=1&group_children=0'
+
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+        page.goto(page_url, timeout=60000)
+                    
+        hotels = page.locator('//div[@data-testid="property-card"]').all()
+        print(f'There are: {len(hotels)} hotels.')
+
+        hotels_list = []
+        for hotel in hotels:
+            hotel_dict = {}
+            hotel_dict['hotel'] = hotel.locator('//div[@data-testid="title"]').inner_text()
+            hotel_dict['price'] = hotel.locator('//span[@data-testid="price-and-discounted-price"]').inner_text()
+            hotel_dict['score'] = hotel.locator('//div[@data-testid="review-score"]/div[1]').inner_text()
+            hotel_dict['avg review'] = hotel.locator('//div[@data-testid="review-score"]/div[2]/div[1]').inner_text()
+            hotel_dict['reviews count'] = hotel.locator('//div[@data-testid="review-score"]/div[2]/div[2]').inner_text().split()[0]
+
+            hotels_list.append(hotel_dict)
+        
+        df = pd.DataFrame(hotels_list)
+        df.to_csv('hotels_list.csv', index=False) 
+        
+        browser.close()
+            
+if __name__ == '__main__':
+    main()
